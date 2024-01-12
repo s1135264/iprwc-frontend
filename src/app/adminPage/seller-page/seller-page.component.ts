a2786ab165c5817f45a6ecb9f90741f296242150
FLYXINATOR <102964602+s1135264@users.noreply.github.com>
2024-01-12 22:57:10 +0100

    Added: Sellers page

--- src/app/adminPage/seller-page/seller-page.component.ts
+++ src/app/adminPage/seller-page/seller-page.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {NavigationBarComponent} from "../../dashboard/navigation-bar/navigation-bar.component";
+import {HttpClient} from "@angular/common/http";
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-seller-page',
   standalone: true,
-  imports: [],
+  imports: [
+    NavigationBarComponent
+  ],
   templateUrl: './seller-page.component.html',
   styleUrl: './seller-page.component.css'
 })
 export class SellerPageComponent {
 
+
+  constructor(private http: HttpClient, private cookie: CookieService) {
+  }
 }
