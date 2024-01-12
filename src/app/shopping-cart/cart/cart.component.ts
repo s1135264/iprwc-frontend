33d01f446a97d6e7d36dc35d8a8ffabb8f4eeeee
FLYXINATOR <102964602+s1135264@users.noreply.github.com>
2024-01-12 21:49:59 +0100

    Added: show products in cart

--- src/app/shopping-cart/cart/cart.component.ts
+++ src/app/shopping-cart/cart/cart.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import {NavigationBarComponent} from "../../dashboard/navigation-bar/navigation-bar.component";
+import {NgForOf} from "@angular/common";
+import {CartItemComponent} from "../cart-item/cart-item.component";
+import {CookieService} from "ngx-cookie-service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [
+    NavigationBarComponent,
+    NgForOf,
+    CartItemComponent
+  ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+  cart:any = [];
+
+
+  constructor(private http: HttpClient, private cookie: CookieService) {
+
+    setTimeout(() => {
+      this.getCart();
+    } , 10);
+  }
+
+
+  //get cart from server with token
+  baseUrl = "http://127.0.0.1:8080/"
+  endpointGetCart = "api/v1/cart/get"
+  getCart(){
+    let url = this.baseUrl + this.endpointGetCart;
+    let data = this.cookie.get("token");
+
+    this.http.post(url, data).subscribe((response) =>{
+      this.cart = response;
+    }, (error) =>{
+      console.log(error);
+    });
+  }
+
 
 }
